7cb60befdcbbeab6f941eb2b234ae2afc041c69d
kkdzv <kkdzv@bk.ru>
2022-10-16 00:39:15 +0300

    add pip_requirements and was changed journal/urls

--- journal/urls.py
+++ journal/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('journal/list', views.JournalListView.as_view(),
+  path('', views.JournalListView.as_view(),
        name="journallist"),
-  path('journal/journalcreate', views.JournalCreateView.as_view(),
+  path('journalcreate', views.JournalCreateView.as_view(),
        name="createjournal"),
-  path('journal/journalupdate/<pk>', views.JournalUpdateView.as_view(),
+  path('journalupdate/<pk>', views.JournalUpdateView.as_view(),
        name="updatejournal"),
-  path('journal/journaldelete/<pk>', views.JournalDeleteView.as_view(),
+  path('journaldelete/<pk>', views.JournalDeleteView.as_view(),
        name="deletejournal"),
 ]
